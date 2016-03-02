7f30619c474a836c450bba6b72b603a5b27ad6f3
pydev <xtfkpi@gmail.com>
2016-03-02 16:52:55 +0200

    updates

--- 01.py
+++ 01.py
@@ -1,35 +1,37 @@
-import itertools,numpy, har
-def transit(c,v):
-    return c,v
-def adsorb(c,v):
-    # print("concentration ",c)
-    return 1-0.4*numpy.exp(c),v
-def mech(c,v):
-    return 40-0.7*c,v
-def revers(c,v):
-    r=0.83
-    return (1-r)*c,v/(1+1/r)
+from blocks import *
+import har
+
+
 def best_scheme():
-    dmax=ds[0]
-    imax=0
+    dmax = ds[0]
+    imax = 0
     for i in range(len(ds)):
-        if ds[i]>dmax:
-            dmax=ds[i]
-            imax=i
+        if ds[i] > dmax:
+            dmax = ds[i]
+            imax = i
     return schemes[imax]
 
-funcs = [mech,revers,transit]
-# random.uniform(1,10)
-cs,vs,ds,schemes = [],[],[],[]
-for scheme in itertools.product(funcs, repeat=3):
-    c,v=38,1
-    assert len(scheme)>0
-    for node in scheme:
-        c,v = node(c,v)
-    schemes.append(scheme)
-    cs.append(c)
-    vs.append(v)
-    ds.append(har.d(c,v))
 
-#ttprint(cs)
-print(best_scheme())
+C_START = 35
+funcs = [mech, adsorb,revers, transit]
+# random.uniform(1,10)
+cs, vs, ds, schemes = [], [], [], []
+for scheme in itertools.product(funcs, repeat=3):
+    if not is_valid(scheme):
+        continue
+    c, v = C_START, 1
+    for node in scheme:
+        c0 = c
+        c, v = node(c, v)
+        if (c < 0) or (c > c0):
+            break
+
+    if 0 < c < C_START:
+        print('---------------')
+        # print(c>0 and c< C_START)
+        print(c)
+        print('calculated for ' + str(del_transit(scheme)))
+        schemes.append(scheme)
+        cs.append(c)
+        vs.append(v)
+        ds.append(har.d(c, v))
